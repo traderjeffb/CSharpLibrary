71100a9de7134318c786fe64100d869473321ad0
Jeff Beeson <traderjeffb@gmail.com>
2017-11-02 05:18:45 -0400

    added practice files

---
+++ 1.00-if_else_Practice/1.01_Single_Dimention_Arrays_Practice/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1._01_Single_Dimention_Arrays_Practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ 1.00-if_else_Practice/1.02_Using_Foreach_Loops_with_Arrays/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1._02_Using_Foreach_Loops_with_Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
